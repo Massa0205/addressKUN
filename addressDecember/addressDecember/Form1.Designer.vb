c0cbdac36e4f23f0c0dad4099c9d3e0b4dd15fb5
MasaLaptop <nmasataka0205@gmail.com>
2021-11-17 22:38:18 +0900

    wip creating Main form

--- addressDecember/addressDecember/Form1.Designer.vb
+++ addressDecember/addressDecember/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class frmMain
     Inherits System.Windows.Forms.Form
 
     'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
@@ -22,10 +22,54 @@ Partial Class Form1
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.pnlLeftBar = New System.Windows.Forms.Panel()
+        Me.pnlSearchBar = New System.Windows.Forms.Panel()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.pnlSearchBar.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pnlLeftBar
+        '
+        Me.pnlLeftBar.BackColor = System.Drawing.Color.FromArgb(CType(CType(238, Byte), Integer), CType(CType(238, Byte), Integer), CType(CType(238, Byte), Integer))
+        Me.pnlLeftBar.Location = New System.Drawing.Point(0, 0)
+        Me.pnlLeftBar.Name = "pnlLeftBar"
+        Me.pnlLeftBar.Size = New System.Drawing.Size(275, 683)
+        Me.pnlLeftBar.TabIndex = 0
+        '
+        'pnlSearchBar
+        '
+        Me.pnlSearchBar.BackColor = System.Drawing.Color.FromArgb(CType(CType(238, Byte), Integer), CType(CType(238, Byte), Integer), CType(CType(238, Byte), Integer))
+        Me.pnlSearchBar.Controls.Add(Me.TextBox1)
+        Me.pnlSearchBar.Location = New System.Drawing.Point(0, 0)
+        Me.pnlSearchBar.Name = "pnlSearchBar"
+        Me.pnlSearchBar.Size = New System.Drawing.Size(274, 63)
+        Me.pnlSearchBar.TabIndex = 0
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(53, 22)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(160, 19)
+        Me.TextBox1.TabIndex = 0
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(1264, 681)
+        Me.Controls.Add(Me.pnlSearchBar)
+        Me.Controls.Add(Me.pnlLeftBar)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Name = "frmMain"
         Me.Text = "Form1"
+        Me.pnlSearchBar.ResumeLayout(False)
+        Me.pnlSearchBar.PerformLayout()
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents pnlLeftBar As Panel
+    Friend WithEvents pnlSearchBar As Panel
+    Friend WithEvents TextBox1 As TextBox
 End Class
